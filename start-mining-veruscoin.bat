4ce4b20963d9f3fe18d5bd3c15ea2c42e6828ac3
Samsul-Arif <samsulsusilo145@gmail.com>
2024-12-30 04:47:52 +0700

    Add files via upload

---
+++ start-mining-veruscoin.bat
@@ -0,0 +1,5 @@
+@echo off 
+cd %~dp0 
+ 
+cls 
+SRBMiner-MULTI.exe --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RMgowJede2mznhkhES4KAwWFVUCvAAL4jH --Worker VerusPC --password Hybrid --cpu-threads 0
